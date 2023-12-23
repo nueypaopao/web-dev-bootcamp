b8bb23a6c700533fbc7058a5c12b7de0e2568bd6
Paopao <nuey_paopao@hotmail.co.th>
2023-12-23 14:45:04 +0700

    header text

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#DEF81D",
+        primary: "rgb(192, 132, 252)",
         "wd-white": "#FFFFFF",
         "wd-grey": "#999999",
         "wd-darkgrey": "#2D2D2D",
@@ -12,8 +12,11 @@ module.exports = {
       },
       backgroundImage: {
         "hero-pattern":
-          "linear-gradient(to left, rgba(0,0,0, 0), rgba(0,0,0,1)), url('/assets/hero/hero.jpg')",
+          "linear-gradient(to left, rgba(0,0,0, 0), rgba(0,0,0,1)), url('../assets/hero/hero.jpg')",
       },
+      fontFamily:{
+        'sans':['poppins', 'Arial', 'sans-serif']
+      }
     },
   },
   plugins: [],
